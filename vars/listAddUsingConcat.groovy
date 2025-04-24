65f47c2c9adb772e19f6c869fd51db088d9f6d9a
prin073 <me.kumarprince@gmail.com>
2025-04-24 22:46:27 +0530

    share lib

---
+++ vars/listAddUsingConcat.groovy
@@ -0,0 +1,21 @@
+//remember that @NonCPS methods can't use Groovy methods that are restricted by the sandbox, such as add().
+// This leads to the same sandbox error.
+//won't work
+/*
+@NonCPS
+def call(String valueToAdd, List args = []) {
+    def newList = args
+    newList.add(valueToAdd) //1. using method
+
+    newList.each { arg ->
+        println("${arg}")  // Use double quotes forLoop GString
+    }
+}
+*/
+
+def call(String valueToAdd, List args = []) {
+    def newList = args + [valueToAdd] // Concatenate the valueToAdd to args
+    newList.each { arg ->
+        println("${arg}")  // This will print values in the non-CPS context
+    }
+}

a97b768357414d501267b43a7283b7e851d98e5c
prin073 <me.kumarprince@gmail.com>
2025-04-24 14:45:23 +0530

    share lib

---
+++ vars/forEach.groovy
@@ -0,0 +1,6 @@
+
+def call(List args = []) {
+    args.each { arg ->
+        println("${arg}")  // Use double quotes forLoop GString
+    }
+}
--- vars/helloWorldListForEach.groovy
+++
@@ -1,6 +0,0 @@
-
-def call(List args = []) {
-    args.each { arg ->
-        println("${arg}")  // Use double quotes for GString
-    }
-}

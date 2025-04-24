c7058d4c2eb63fa6d6487880556416768714fd14
prin073 <me.kumarprince@gmail.com>
2025-04-24 09:01:32 +0530

    shared_lib

--- vars/helloWorldMap.groovy
+++ vars/helloWorldMap.groovy
@@ -1,22 +1,20 @@
 
-//This won't work anymore with newer jenkins version. Jenkins Sandbox has tighten it's rules
-
+//1. simple way
+/*
 def call(Map args = [:]) {
     sh "echo Hello World! My name is ${args.name} and today is ${args.weekOfDay}"
 }
+*/
 
 
 
-/*
-// new and accepted version
+//2. new and accepted version
 def call(Map args = [:]) {
+//    Above will work but it's better to assign some default values
 //    def name = args.name
 //    def weekOfDay = args.weekOfDay
-
-    // Above will work but it's better to assign some default values
 
     def name = args.get('name', 'Unknown')
     def weekOfDay = args.get('weekOfDay', 'Some day')
     sh "echo Hello World! My name is ${name} and today is ${weekOfDay}"
 }
-*/
